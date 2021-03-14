d445a74a727bb397a0088500349c7e6c862aef7a
Abdisamed <therealaa86@gmail.com>
2021-03-14 17:22:59 -0400

    Upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { TodayComponent } from './today/today.component';
+
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodayComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBcLdl7Cok-H6pkNjcz2F3kNdIBY0ckty4'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
